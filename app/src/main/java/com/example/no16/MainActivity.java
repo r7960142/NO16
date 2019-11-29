5bc45839f7bbeef6ca17a8343b49c08697c9e969
r7960142 <id0938237286@gmail.com>
2019-11-29 11:54:24 +0800

    靜態

--- app/src/main/java/com/example/no16/MainActivity.java
+++ app/src/main/java/com/example/no16/MainActivity.java
@@ -21,13 +21,7 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.textView);
         listView = (ListView) findViewById(R.id.listview);
         listView.setOnItemClickListener(listViewOnClickListener);
-        //ListView 要顯示的內容
-        String[] str = {"新北市","台北市","台中市","高雄市"};
-        //android.R.layout.simple_list_item_1 為內建樣式，還有其他樣式可自行研究
-        ArrayAdapter adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,
-                str);
-        listView.setAdapter(adapter);
+
     }
 
     private AdapterView.OnItemClickListener listViewOnClickListener
